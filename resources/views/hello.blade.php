d1057a13326b3bbeec5cedc18191425f34962dd0
khaledQwash <khaled2002qgmail.com>
2026-02-14 12:17:56 +0300

    second commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first Assignment</title>
+    <style>
+        .main-div{
+            height: 300px;
+            text-align: center;
+            background-color: #eeee;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        h1{
+            color: #2c2c2c;
+
+        }
+    </style>
+</head>
+<body>
+    <div class="main-div">
+        <h1>
+            Hi, From Khaled
+        </h1>
+    </div>
+</body>
+</html>
